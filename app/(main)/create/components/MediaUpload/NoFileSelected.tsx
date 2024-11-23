416f58b41ac66c48891efcfab8636b0c573d510f
Tomas <mantoketo4@gmail.com>
2024-11-23 19:44:10 +1000

    change color

--- app/(main)/create/components/MediaUpload/NoFileSelected.tsx
+++ app/(main)/create/components/MediaUpload/NoFileSelected.tsx
@@ -2,7 +2,7 @@ import { CloudUpload } from "lucide-react"
 
 const NoFileSelected = ({ onClick }: { onClick: () => void }) => (
   <div
-    className="absolute inset-0 flex flex-col items-center justify-center space-y-2 text-black cursor-pointer"
+    className="absolute inset-0 flex flex-col items-center justify-center space-y-2 text-black cursor-pointer text-white"
     onClick={onClick}
   >
     <CloudUpload className="w-8 h-8" />
