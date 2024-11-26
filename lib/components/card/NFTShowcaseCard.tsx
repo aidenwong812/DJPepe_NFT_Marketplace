a0aa8d424e8533456790c95df228f97f97f155e8
Tomas <mantoketo4@gmail.com>
2024-11-26 19:44:37 +1000

    add gap

--- lib/components/card/NFTShowcaseCard.tsx
+++ lib/components/card/NFTShowcaseCard.tsx
@@ -20,7 +20,7 @@ const NFTShowcaseCard: FC<Props> = ({ asset, nftId, name, price }) => {
     <Card
       isFooterBlurred
       radius="lg"
-      className="flex justify-center items-center border-none p-2 bg-slate-500/40"
+      className="flex justify-center items-center border-none p-2 mb-4 bg-slate-500/40"
     >
       <Image
         alt="Woman listing to music"
@@ -30,7 +30,7 @@ const NFTShowcaseCard: FC<Props> = ({ asset, nftId, name, price }) => {
         onClick={() => router.push(`/nft/${nftId}`)}
         onLoad={() => setLoading(true)}
       />
-      <div className="flex flex-wrap justify-between w-full px-16 py-3">
+      <div className="flex flex-col flex-wrap justify-between w-full px-16 py-3">
         <p className="text-lg font-semibold font-maladroit">{name}</p>
         <p className="text-lg font-semibold font-maladroit">{Number(price)/10**18} ETH</p>
       </div>
